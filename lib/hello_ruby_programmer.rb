2e6c9bc12e642a2fc089e06df7a92141e6c360d9
Phill McGee <entrenest@outlook.com>
2020-05-27 16:53:14 +0000

    Automatically backed up by Learn

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -2,7 +2,9 @@ def greeting
   puts ("Hi, " name "! Welcome to the wonderful world of Ruby programming."
 end
 
+def name
 puts "Please enter your name so that we can greet you more personally:"
 name = gets.strip
+end
 
 greeting(name)
